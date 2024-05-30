fa8a8df43f3e41a97f04c9d70a3689f18d75f009
Luca Castellaro <luca_castellaro@outlook.com>
2024-05-30 21:13:41 +0200

    Base handler

---
+++ CNAS.Business/Handlers/BaseHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace CNAS.Business.Handlers;
+
+/// <summary>
+/// The base class for creating a Handler.
+/// </summary>
+/// <typeparam name="TRequest">The type of the request.</typeparam>
+/// <typeparam name="TResponse">The type of the response.</typeparam>
+/// <seealso cref="MediatR.IRequestHandler&lt;TRequest, TResponse&gt;" />
+public abstract class BaseHandler<TRequest, TResponse> : IRequestHandler<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    /// <summary>
+    /// The logger.
+    /// </summary>
+    protected readonly ILogger<BaseHandler<TRequest, TResponse>> _logger;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BaseHandler{TRequest, TResponse}"/> class.
+    /// </summary>
+    /// <param name="logger">The logger.</param>
+    protected BaseHandler(ILogger<BaseHandler<TRequest, TResponse>> logger)
+    {
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// Handles the specified request.
+    /// </summary>
+    /// <param name="req">The request.</param>
+    /// <param name="ct">The cancellation token.</param>
+    /// <returns><c>Task<TResponse></c></returns>
+    public abstract Task<TResponse> Handle(TRequest req, CancellationToken ct);
+}
